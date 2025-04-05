02185d93912d4fa9433c2e4ae9815fa1f1794f2c
shafiqulislamsagor-developer <shafiqul.islam.sagor.developer@gmail.com>
2025-04-05 23:20:56 +0600

    boolean data type print and header includeing

---
+++ Module-1/bool.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(){
+    bool b;
+    b = false;
+    printf("%d",b);
+    return 0;
+}
--- Module-1/first.c
+++ Module-1/first.c
@@ -7,10 +7,10 @@ int main(){
     // printf("\n\tHei !");
 
     // variables and data types structures
-    int roll = 10;
-    float marks = 85.5;
-    char name = 'A';
-    printf("Roll: %d\nMarks:%.1f\nName: %c",roll,marks ,name);
+    // int roll = 10;
+    // float marks = 85.5;
+    // char name = 'A';
+    // printf("Roll: %d\nMarks:%.1f\nName: %c",roll,marks ,name);
     // printf("%c", name);
 
     return 0;
