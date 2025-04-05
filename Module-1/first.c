09e9f24ae1dc0d626e525a54d8c54d04232a965a
shafiqulislamsagor-developer <shafiqul.islam.sagor.developer@gmail.com>
2025-04-05 23:13:04 +0600

    variable and data types structure printing format

--- Module-1/first.c
+++ Module-1/first.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 int main(){
     // All Code Store
-    printf("Hello ,\n world!");
-    printf("\n\tHei !");
+
+    // code printing >
+    // printf("Hello ,\n world!");
+    // printf("\n\tHei !");
+
+    // variables and data types structures
+    int roll = 10;
+    float marks = 85.5;
+    char name = 'A';
+    printf("Roll: %d\nMarks:%.1f\nName: %c",roll,marks ,name);
+    // printf("%c", name);
+
     return 0;
 }
