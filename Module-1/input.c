79e3f53501a0aefe35fd84d7d8203ede67935a67
shafiqulislamsagor-developer <shafiqul.islam.sagor.developer@gmail.com>
2025-04-06 00:10:43 +0600

    extra 2 data type learning

--- Module-1/input.c
+++ Module-1/input.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 int main(){
-    int a;
-    float b;
-    char c;
-    scanf("%d %f %c",&a,&b,&c);
-    printf("%d %.2f %c",a,b,c);
+    // int a;
+    // float b;
+    // char c;
+    // scanf("%d %f %c",&a,&b,&c);
+    // printf("%d %.2f %c",a,b,c);
+    long long int a = 1000000000000000000;
+    printf("%lld\n",a);
     return 0;
 }
