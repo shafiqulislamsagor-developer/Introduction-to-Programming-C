65ec03b70cbe492d070c1f2059204cec11309bd7
shafiqulislamsagor-developer <shafiqul.islam.sagor.developer@gmail.com>
2025-04-06 21:47:39 +0600

    module 2 arithmetic oparator

---
+++ Module-2/arithmetic.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+    int a = 5;
+    float b = 2;
+    int sum = a + b;
+    // printf("sum = %d\n",sum);
+    int sub = a - b;
+    // printf("sub = %d\n",sub);
+    int mul = a * b;
+    // printf("mul = %d\n",mul);
+    float div = a / b;
+    printf("div = %f\n",div);
+    // int mod = a % b;
+    // printf("mod = %d\n",mod);
+    return 0;
+}
---
+++ Module-2/tempCodeRunnerFile.c
@@ -0,0 +1,2 @@
+
+    int sub = a - b;
