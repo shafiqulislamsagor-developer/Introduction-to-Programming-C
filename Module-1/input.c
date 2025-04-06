e8df70b4bd5c8de93e7db0fa242889d04c843c96
shafiqulislamsagor-developer <shafiqul.islam.sagor.developer@gmail.com>
2025-04-06 11:23:27 +0600

    not print text.c file code.. solve next time

--- Module-1/input.c
+++ Module-1/input.c
@@ -7,6 +7,6 @@ int main(){
     // scanf("%d %f %c",&a,&b,&c);
     // printf("%d %.2f %c",a,b,c);
     long long int a = 1000000000000000000;
-    printf("%lld\n",a);
+    printf("This string contains a percent sign %%.\n");
     return 0;
 }
