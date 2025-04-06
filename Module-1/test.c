85cb8bcb352739e1f2b49bc8cf83c20139c5e27d
shafiqulislamsagor-developer <shafiqul.islam.sagor.developer@gmail.com>
2025-04-06 22:17:41 +0600

    fixed module 1 problem tommorow

--- Module-1/test.c
+++ Module-1/test.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 
 int main(){
-    // printf("This is a line with a newline character\n");
-    //   printf("This is a line with a horizontal tab\tright here.\n");
-    //   printf("This string contains a \"double quote\"\n");
-    //   printf("This string contains a \'single quote\'\n");
-    //   printf("This string contains a \\backslash\\\n");
+    printf("This is a line with a newline character\n");
+    printf("This is a line with a horizontal tab\tright here.\n");
+    printf("This string contains a \"double quote\"\n");
+    printf("This string contains a \'single quote\'\n");
+    printf("This string contains a \\backslash\\\n");
     //   printf("This string contains a percent sign %%.\n");
     
     return 0;
 }
+// #include <stdio.h>
+// int main()
+// {
+//     printf("Hello World\n");
+//     printf("This is a line with a horizontal tab\tright here.\n");
+//     printf("This string contains a \'single quote\'\n");
+//     return 0;
+// }
