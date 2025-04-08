4b7d760527ce3f8db2526b9650b52179cd75f81f
shafiqulislamsagor-developer <shafiqul.islam.sagor.developer@gmail.com>
2025-04-08 16:36:35 +0600

    solve some code

--- Module-2/mod_operator.c
+++ Module-2/mod_operator.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 int main(){
-    int a = 18;
-    int b = 4;
-    int mod = a % b;
-    printf("mod: %d\n", mod);
+    // int a = 15;
+    // int b = 4;
+    // double mod = 15%4;
+    // printf("mod: %lf\n", mod);
+    int a = 10;
+    int b = 12;
+    if(a>=b || a<=b){
+        printf("hi ");
+    }
+    printf("hello");
     return 0; 
 }
