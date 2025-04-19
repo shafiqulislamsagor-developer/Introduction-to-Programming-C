8066669c9d39323a17c74606afa47bfc5fdc67ee
shafiqulislamsagor-developer <shafiqul.islam.sagor.developer@gmail.com>
2025-04-20 01:26:35 +0600

    test perpus logic build

--- Module-1/test1.c
+++ Module-1/test1.c
@@ -1,7 +1,33 @@
 #include<stdio.h>
 int main(){
     int a;
-    // code
     scanf("%d",&a);
-    printf("%d",a);
+    int Arr[a];
+    for(int i = 0;i < a;i++){
+        int value;
+        scanf("%d",&value);
+        Arr[i] = value;
+    }
+    int Arr2[a];
+    int index , value;
+    scanf("%d %d",&index,&value);
+    for (int i = 0; i < a; i++)
+    {
+        if(i == index){
+            Arr2[i]= value;
+
+        }
+        else if (index < i)
+        {
+            Arr2[i] = Arr[i-1];
+        }
+        else{
+            Arr2[i]= Arr[i];
+        }
+    }
+    for (int i = 0; i < a; i++)
+    {
+        printf("%d ",Arr2[i]);
+    }
+    
 }
