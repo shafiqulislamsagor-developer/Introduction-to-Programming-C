df5cc96e44126f2f496ca06749ccd8219e47edba
shafiqulislamsagor-developer <shafiqul.islam.sagor.developer@gmail.com>
2025-05-25 00:34:02 +0600

    some code learn c++

---
+++ CPP_module/print.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+int main(){
+    // std :: cout << "hello world";
+    int x = 10;
+    char y = 'H';
+    std :: cout << x << "tk dibo tmk okay" << y << std :: endl;
+    return 0;
+}
